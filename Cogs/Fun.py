417a822c831b1cb36f5ea97b25ec6afefc6d2e1d
Sasen Perera <sasen.learnings@gmail.com>
2022-01-16 11:03:45 +0530

    Added Quiz and Quiz add command

--- Cogs/Fun.py
+++ Cogs/Fun.py
@@ -1,11 +1,25 @@
+import asyncio
 import discord
 from discord.ext import commands
-from random import randint, choice
+from random import randint, randrange
+import pymongo
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+if os.path.isfile('./.env'):
+    load_dotenv('Cogs/.env')
+else:
+    pass
+
+psw = os.environ['mongopsw']
+client = MongoClient(f"mongodb+srv://QuizWriter:{psw}@pokequiz.vi6j1.mongodb.net/PokeQuiz?retryWrites=true&w=majority")
+db = client.Quiz
+collection = db.PokeQuiz
 
 class Fun(commands.Cog):
     def __init__(self, bot:commands.Bot):
         self.bot = bot
-
 
     @commands.command(name = "coin",
                     usage="@the_person_who_chose_heads(optional) @the_person_who_chose_tails(optional)",
@@ -36,6 +50,46 @@ class Fun(commands.Cog):
             dice = randint(1, sides)
             await ctx.send(f"A 🎲({sides}) is rolled and lands on {dice}")
 
+    @commands.command(name= "quiz",
+                    brief="asks a random pokemon related quiz",
+                    description="Asks a Pokemon related question from the database")
+    async def quiz(self, ctx:commands.Context):
+        document = list(collection.find({}, {'_id': 0}))
+        qn = randint(0, len(document)-1)
+        question = document[qn]
+        color = f'{randrange(16**2):x}{randrange(16**2):x}{randrange(16**2):x}'
+        color = int(color, 16)
+        embed = discord.Embed(title=f"[{qn+1}]{question.get('question')}", description="Answer this you have 30 secs", colour=color)
+        await ctx.send(embed=embed)
+        def check(m):
+            msg = m.content
+            if '/' in question.get('answer'):
+                answer = str(question.get('answer'))
+                answer = answer.split('/')
+            return msg.lower() == answer[0] or msg.lower() == answer[1]
+        try:
+            mesg = self.bot.wait_for('message', timeout=30.0, check=check)
+        except asyncio.TimeoutError:
+            ctx.send("Time Out!")
+        else:
+            ctx.send(f"You got it {mesg.author}, answer(s) are {question.get('answer')}")
+
+        
+
+    @commands.command(name="add_quiz",
+                    brief="Adds a pokemon quiz",
+                    description="Adds a pokemon related quiz to the database",
+                    usage="Question Answer")
+    async def add_quiz(self, ctx:commands.Context, question: str, answer: str):
+        question = question.lower()
+        answer = answer.lower()
+        myquery = { "question": question }
+        if collection.count_documents(myquery) == 0:
+            post = {"question": question,
+                    "answer": answer}
+            collection.insert_one(post)
+        else:
+            ctx.send("This question is already in the database ")
 
 def setup(bot:commands.Bot):
     bot.add_cog(Fun(bot))
